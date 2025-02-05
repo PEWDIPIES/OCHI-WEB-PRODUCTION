0c2d7f42f4cb46b875587b955d8291485a5853ee
hunain soomro <huniiali1122@gmail.com>
2025-02-05 21:32:37 +0500

    Your commit message here

--- src/Components/pages/Aboutus/AboutusMarqueeandteam/Aboutteam.jsx
+++ src/Components/pages/Aboutus/AboutusMarqueeandteam/Aboutteam.jsx
@@ -1,42 +1,37 @@
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCube, Pagination } from 'swiper/modules';
 
 const AboutTeam = () => {
     return (
-        <>
         <div className="Aboutteam">
-        <Swiper
-            effect={'cube'}
-            grabCursor={true}
-            cubeEffect={{
-              shadow: true,
-              slideShadows: true,
-              shadowOffset: 20,
-              shadowScale: 0.94,
-            }}
-            pagination={true}
-            modules={[EffectCube, Pagination]}
-            className="mySwiper"
-          >
-            <SwiperSlide>
-              <img src="https://ochi.design/wp-content/uploads/2023/08/Group-4683-304x330.jpg" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="https://ochi.design/wp-content/uploads/2022/05/Olena-Zderka-2-304x330.png" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="https://ochi.design/wp-content/uploads/2022/05/IMG_3112-1-304x330.jpg" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="https://ochi.design/wp-content/uploads/2022/05/Kseniia-Palamarchuk-304x330.jpg" />
-            </SwiperSlide>
-          </Swiper>
+            <Swiper
+                effect={'cube'}
+                grabCursor={true}
+                cubeEffect={{
+                    shadow: true,
+                    slideShadows: true,
+                    shadowOffset: 20,
+                    shadowScale: 0.94,
+                }}
+                pagination={true}
+                modules={[EffectCube, Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <img src="https://ochi.design/wp-content/uploads/2023/08/Group-4683-304x330.jpg" alt="Team Member 1" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://ochi.design/wp-content/uploads/2022/05/Olena-Zderka-2-304x330.png" alt="Team Member 2" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://ochi.design/wp-content/uploads/2022/05/IMG_3112-1-304x330.jpg" alt="Team Member 3" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://ochi.design/wp-content/uploads/2022/05/Kseniia-Palamarchuk-304x330.jpg" alt="Team Member 4" />
+                </SwiperSlide>
+            </Swiper>
         </div>
-     
-        </>
-      );
- 
+    );
 }
 
 export default AboutTeam;
